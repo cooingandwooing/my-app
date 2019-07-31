fd09265752e5ee17335b2134a0ab9ff8d2f52a67
qingyejiazhu <qingyejiazhu@163.com>
2019-07-31 11:45:30 +0800

    it

---
+++ src/pages/router_demo/router1/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { HashRouter, Route, Link, Switch } from 'react-router-dom';
+import Main from './Main';
+import Topic from './topic';
+import About from './about';
+
+export default class Home extends React.Component {
+
+    render() {
+        return (
+            // 必须有个根节点 允许加载多个路由（除非设置了 exact）
+            <HashRouter>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/topics">Topics</Link>
+                        </li>
+                    </ul>
+
+
+                    <hr />
+
+                    <Switch>
+                        <Route exact path="/" component={Main} />
+                        <Route path="/about" component={About} />
+                        <Route path="/topics" component={Topic} />
+                    </Switch>
+                </div>
+            </HashRouter>
+        );
+
+    }
+}
